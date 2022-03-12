642209834e81e20d751ec81fea891c5148a56d9d
sandun23 <sandunjanitha8@gmail.com>
2022-03-12 09:50:23 +0530

    Build an Attack Done

---
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour
+{
+
+    bool attaking = false;
+    public ParticleSystem BulletParticleSystem;
+
+    private ParticleSystem.EmissionModule em;
+
+    float AttackTimer = 0f;
+
+    private float FiringRate = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        em = BulletParticleSystem.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attaking = Input.GetMouseButton(0);
+
+        AttackTimer += Time.deltaTime;
+
+        if (attaking && AttackTimer >= 1f/FiringRate)
+        {
+            AttackTimer = 0f;
+            Attack();
+        }
+
+        em.rateOverTime = attaking ? FiringRate : 0f;
+    }
+
+
+    void Attack()
+    {
+        Ray ray = new Ray(BulletParticleSystem.transform.position, BulletParticleSystem.transform.forward);
+    }
+}
